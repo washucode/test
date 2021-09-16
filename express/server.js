2f7347bfaf336636caedf0a7f9b8b588ee8a31e9
washucode <estherw8525@gmail.com>
2021-09-16 16:50:30 +0300

    config17

--- express/server.js
+++ express/server.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // Link to views folder.
 let views = path.join(__dirname, '../dist');
-app.use(express.static('static'));
+app.use(express.static('assets'));
 
 // Home route.
 router.get('/', (req, res) => {
